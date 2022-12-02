5f68acddafdbde7f0f5a9ca9ef6cfc75fa1cc855
Mario D'Andrea <mariodandrea2595@gmail.com>
2022-12-02 02:07:38 +0100

    solving exercise 3 about arrow function

---
+++ exercise-3/exercise-3.js
@@ -0,0 +1,25 @@
+const sum = function (a, b) {
+  return a + b;
+}
+
+const subtract = function (a, b) {
+  return a - b;
+}
+
+const multiply = function (a, b) {
+  return a * b;
+}
+
+const divide = function (a, b) {
+  return a / b;
+}
+
+const log = function (value) {
+  console.log(value);
+}
+
+const operation = () => {
+  return divide(subtract(multiply(sum(2, 4), sum(5, 2)), 2), 5);
+}
+
+log(operation());
